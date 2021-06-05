86a8a580ccde1ea41d68e1ac1db52abdd1dc6ecf
Nikita Kovach <snagituy@gmail.com>
2021-06-05 17:24:05 +0300

    last update

---
+++ data.js
@@ -0,0 +1,25 @@
+module.exports = function(){
+    return{
+      products: [
+        { id: 1, name: "<iframe src=\"https://open.spotify.com/embed/album/2FRBG35Iyble4XL9AVg9qf\" width=\"300\" height=\"380\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>", category: "Watersports",
+            description: "A boat for one person", price: 275 },
+        { id: 2, name: "<iframe src=\"https://open.spotify.com/embed/album/2FRBG35Iyble4XL9AVg9qf\" width=\"300\" height=\"380\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>", category: "Watersports",
+            description: "Protective and fashionable", price: 48.95 },
+        { id: 3, name: "<iframe src=\"https://open.spotify.com/embed/album/2FRBG35Iyble4XL9AVg9qf\" width=\"300\" height=\"380\" frameborder=\"0\" allowtransparency=\"true\" allow=\"encrypted-media\"></iframe>", category: "Soccer", 
+            description: "FIFA-approved size and weight", price: 19.50 },
+        { id: 4, name: "", category: "Soccer", 
+            description: "Give your playing field a proffessional touch", price: 34.95 },
+        { id: 5, name: "", category: "Soccer",
+            description: "Flat-packed 35,000-seat stadium", price: 79500 },
+        { id: 6, name: "", category: "Chess",
+            description: "Improve brain efficiency by 75%", price: 16 },
+        { id: 7, name: "", category: "Chess",
+            description: "Secretly give your opponent a disadvantage", price: 29.95 },
+        { id: 8, name: "", category: "Chess",
+            description: "A fun game for the family", price: 75 },
+        { id: 9, name: "", category: "Chess",
+            description: "Gold-plated, diamond-studded King", price: 1200 }
+      ],
+      orders: []
+    }
+  }
